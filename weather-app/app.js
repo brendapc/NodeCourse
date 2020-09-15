312c49b4da71e7cc71b1ce2bfd9b153632c03f6f
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-09-15 16:26:22 -0300

    weather-app

---
+++ weather-app/app.js
@@ -0,0 +1,11 @@
+const request = require('request')
+
+const city = 'Porto Alegre'
+const key = '5d5fcbee6f54365d079d5fc0d88a2670'
+const url = `http://api.weatherstack.com/current?access_key=${key}&query=${city}`
+
+request({url: url, json: true}, (error, response)=>{
+    console.log(`${response.body.current.weather_descriptions}, it is currently ${response.body.current.temperature} degrees out, it feels like ${response.body.current.feelslike} degrees out!`)
+})
+
+//
