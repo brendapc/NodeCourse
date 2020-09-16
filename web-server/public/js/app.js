9dfc8a5649fead69442201adbe94d68f5544c44c
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-09-16 10:02:07 -0300

    fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('client side javascript')
+
+fetch('http://localhost:3000/weather?address=boston').then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+        }else{
+            console.log(data.forecast)
+            console.log(data.address)
+        }
+    })
+})
