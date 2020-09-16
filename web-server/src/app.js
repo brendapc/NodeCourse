2eac7461f81916c1125cdb5cec4a740916050f9c
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-09-15 21:40:08 -0300

    handlebars (hbs)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,11 +2,36 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.set('view engine', 'hbs') //write exactly that
+
 //path manipulates the directories, (current, goalDirectory)
-app.use(express.static(path.join(__dirname, '../public')))
 //express static is a function that receives static files for display
 //app.use says to express to use that files in root '/'
 
+app.use(express.static(path.join(__dirname, '../public'))) /*even if we're using handlebars for templates, 
+the css and client-side js still have to be in the public directory */
+
+
+
+app.get('', (req, res)=>{
+    res.render('index', {//render views and pass values
+        title: 'OMG a title with handlebars',
+        name: 'my application'
+    })
+})
+app.get('/about', (req,res)=>{
+    res.render('about', {
+        title: 'about me!',
+        info: 'i"m a student'
+    })
+})
+
+app.get('/help', (req,res)=>{
+    res.render('help', {
+        title: 'Help!',
+        message: 'sample helpful message'
+    })
+})
 
 app.listen(3000, ()=>{
     console.log('server up on port 3000')
