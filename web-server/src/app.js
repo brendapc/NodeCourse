9f756a3448ffb19963fdcb9971d9b2fd1768fec8
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-09-15 21:06:30 -0300

    loading html files via express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('hellow express')
-})
+//path manipulates the directories, (current, goalDirectory)
+app.use(express.static(path.join(__dirname, '../public')))
+//express static is a function that receives static files for display
+//app.use says to express to use that files in root '/'
 
-app.get('/help', (req, res)=>{
-    res.send({
-        name: 'brenda',
-        age: 17
-    })
-})
 
 app.listen(3000, ()=>{
     console.log('server up on port 3000')
