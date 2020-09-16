70bfe822dff71d61cc02080716145f0cff97578e
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-09-16 09:10:18 -0300

    partials hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,40 +1,63 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 const app = express()
 
-//path manipulates the directories, (current, goalDirectory)
-//express static is a function that receives static files for display
-//app.use says to express to use that files in root '/'
-
+/*path manipulates the directories, (current, goalDirectory)
+express static is a function that receives static files for display
+app.use says to express to use that files in root '/'
+*/
 app.use(express.static(path.join(__dirname, '../public'))) /*even if we're using handlebars for templates, 
 the css and client-side js still have to be in the public directory */
 
 //HANDLEBARS:
 app.set('view engine', 'hbs') //write exactly that, default search for directory named 'views' 
 
-const viewsPath = path.join(__dirname, '../templates') //custom name directory for views
+const viewsPath = path.join(__dirname, '../templates/views') //custom name directory for views
 app.set('views', viewsPath)
 
+const partialsPath = path.join(__dirname, '../templates/partials')
+hbs.registerPartials(partialsPath)
 
+//ROUTES:
 app.get('', (req, res)=>{
     res.render('index', {//render views and pass values
         title: 'OMG a title with handlebars',
-        name: 'my application'
+        name: 'brenda pereira'
     })
 })
 app.get('/about', (req,res)=>{
     res.render('about', {
         title: 'about me!',
-        info: 'i"m a student'
+        info: 'i"m a student',
+        name: 'brenda pereira'
     })
 })
 
 app.get('/help', (req,res)=>{
     res.render('help', {
         title: 'Help!',
-        message: 'sample helpful message'
+        message: 'sample helpful message',
+        name: 'brenda pereira'
     })
 })
+app.get('/help/*',(req,res)=>{
+    res.render('404', {
+        title: '404',
+        name: 'brenda pereira',
+        errorMessage: 'help page not found'
+    })
+})
+
+// * -> anything that hasn't been declared yet
+app.get('*',(req, res)=>{ 
+    res.render('404', {
+        title: '404',
+        name: 'brenda pereira',
+        errorMessage: ' page not found'
+    })
+})
+
 
 app.listen(3000, ()=>{
     console.log('server up on port 3000')
