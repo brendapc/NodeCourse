fc729092d8e1c204d4ab33d37763f1016ff472d3
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-09-18 16:09:10 -0300

    props of model

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,30 +1,50 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1>27017/task-manager-api',{
     useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true
 })
 
-/* const User = mongoose.model('User', {
+const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true //retira espaços 
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error ('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        default: 18,
+        type: Number,
+        validate(value){
+            if(value<=0){
+                throw new Error('age must be a positive number')
+            }
+        }
     }
 })
 
 const me = new User({
-    name: 'Mariana',
-    age: 17
+    name: '  tobby  ',
+    age: 22,
+    email: '   eu@mail.com  '
 })
 
 me.save().then((me)=>{
     console.log('usuário salvo '+ me)
 }).catch((err)=>{
     console.log(err)
-}) */
+})
 
-const Tasks = mongoose.model('Task',{
+/* const Tasks = mongoose.model('Task',{
     title: String,
     done: Boolean
 })
@@ -36,4 +56,4 @@ const chore = new Tasks({
 
 chore.save().then((chore)=>{
     console.log('chore registed ' + chore)
-})
+}) */
