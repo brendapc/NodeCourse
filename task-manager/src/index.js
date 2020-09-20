2d89897482285f8b474bc54ce3fdbd7ce7b547d7
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-09-20 13:04:25 -0300

    refactor routes with async

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -26,60 +26,79 @@ app.post('/users', async (req, res)=>{
     }) */
 })
 
-app.post('/task',(req, res) => {
+app.post('/task', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(()=>{
+    try{
+        await task.save()
         res.status(201).send(task)
-    }).catch((err)=>{
+    } catch(err){
         res.status(400).send(err)
-        console.log(err)
-    })
+    }
+    
+   
 })
 
-app.get('/users', (req, res)=>{
-    User.find({ }).then((users)=>{
+app.get('/users', async (req, res)=>{
+
+    try{
+        const users = await User.find({})
+        res.send(users)
+    }catch(err){
+        res.status(505).send(err)
+    }
+    /* User.find({ }).then((users)=>{
         res.send(users)
     }).catch((err)=>{
         res.status(400).send()
-    })
+    }) */
 })
 
-app.get('/users/:id', (req, res)=>{
+app.get('/users/:id', async (req, res)=>{
     const _id = req.params.id
-    console.log(req.params.id)
 
-    User.findById(_id).then((user)=>{
-
+    try{
+        const user = await User.findById(_id)
         if(!user){
             return res.status(404).send()
         }
-        
+        res.send(user)
+    } catch(err){
+        res.status(500).send(err)
+    }
+
+    /* User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
         res.send(user)
     
     }).catch((err)=>{
         res.status(500).send()
         console.log(err)
-    })
+    }) */
 })
-app.get('/task', (req, res)=>{
-    Task.find({}).then((task)=>{
-        res.send(task)
-    }).catch((err)=>{
-        res.status(500).send()
-    })
+app.get('/task', async (req, res)=>{
+    
+    try{
+        const tasks = await Task.find({})
+        res.send(tasks)
+    }catch(err){
+        res.status(500).send(err) 
+    }
 })
 
-app.get('/task/:id', (req, res)=>{
+app.get('/task/:id', async (req, res)=>{
     const _id = req.params.id
-    Task.findById(_id).then((task)=>{
+
+    try{
+        const task = await Task.findById(_id)
         res.send(task)
-    }).catch((err)=>{
-        res.status(500).send()
-    })
+    }catch(err){
+        res.status(500).send(err)
+    }
+
 })
-
-
 
 app.listen(port, ()=>{
     console.log('port is on at '+ port)
